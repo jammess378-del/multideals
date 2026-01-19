3fae79e4f4c3fd390db04db014d9accc46fc0e1d
jammess378-del <jammess378@gmail.com>
2026-01-19 21:02:01 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,13 @@
-
+const data = {
+    categories: [
+        {id:1, name:"Amazon Deals"},
+        {id:2, name:"Flipkart Deals"},
+        {id:3, name:"Other Deals"}
+    ],
+    products:[
+        {id:1, categoryId:1, name:"Product A", price:499, image:"https://via.placeholder.com/150"},
+        {id:2, categoryId:1, name:"Product B", price:299, image:"https://via.placeholder.com/150"},
+        {id:3, categoryId:2, name:"Product C", price:899, image:"https://via.placeholder.com/150"},
+        {id:4, categoryId:3, name:"Product D", price:199, image:"https://via.placeholder.com/150"}
+    ]
+};
